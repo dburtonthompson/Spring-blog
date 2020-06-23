c0c79a372d62f4734fe617c1c2ab85bc013e4c69
dburtonthompson <dburtonthompson@gmail.com>
2020-06-23 09:13:58 -0400

    updated index/result.html and blogpost controller

---
+++ techtalentblog/src/main/java/com/win/techtalentblog/BlogPost/BlogPostRepoitory.java
@@ -0,0 +1,7 @@
+package com.win.techtalentblog.BlogPost;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BlogPostRepoitory extends CrudRepository<BlogPost, Long> {
+
+}
